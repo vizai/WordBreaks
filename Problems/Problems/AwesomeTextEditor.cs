b58447002788047afb90db2ee91a349a2458e515
Vijaya Sekhar G <vizai.se@gmail.com>
2016-09-24 11:48:36 -0700

    Project Created..Implemented

---
+++ Problems/Problems/AwesomeTextEditor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems
+{
+  public class AwesomeTextEditor
+    {
+        public string GetStringWithLineBreaks( string input,int column)
+        {
+            string[] words = input.Split(' ');   
+            string finalString = string.Empty;
+            string line = string.Empty;
+
+            foreach (var word in words)
+            {
+                if (line.Length + word.Length < column)
+                {
+                    line += string.IsNullOrEmpty(line) ? word : " " + word;
+                }
+                else
+                {
+
+                    finalString = finalString + line + "\n";
+                    line = word;
+                }
+            }
+
+            finalString += line;
+            return finalString;
+        }
+
+    }
+}
---
+++ Problems/ProblemsTests/AwesomeTextEditorTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Problems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.Tests
+{
+    [TestClass()]
+    public class AwesomeTextEditorTests
+    {
+        [TestMethod()]
+        public void GetStringWithLineBreaksTest()
+        {
+            string input = "The quick brown fox jumped over the lazy dog.";
+            string expectedResult = "The quick brown\nfox jumped over\nthe lazy dog.";
+            int column = 15;
+            AwesomeTextEditor obj = new Problems.AwesomeTextEditor();
+            string actualResult=obj.GetStringWithLineBreaks(input, column);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
